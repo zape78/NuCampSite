9193863195bddf5821a5d581e765d90a268124d8
Brian Zapien <brianzapien@gmail.com>
2023-02-25 11:25:56 -0800

    AnimatedDisplayCard

---
+++ Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/features/campsites/campsitesSlice.js
@@ -0,0 +1,13 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+};
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
---
+++ Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/features/partners/partnersList.js
@@ -0,0 +1,25 @@
+import { Col } from 'reactstrap';
+import Partner from "./partner";
+import { selectAllPartners } from './partnersSlice';
+
+
+const PartnersList = () => {
+    const partners = selectAllPartners();
+    return (
+        <Col>
+            {partners.map(
+                ((partner) => {
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+
+                            <Partner partner={partner} />
+                        </div>
+                    )
+                })
+            )}
+        </Col>
+    )
+}
+
+
+export default PartnersList
