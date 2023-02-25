e55a9bb2e691e0247e87f8f6836db4203add1740
Brian Zapien <brianzapien@gmail.com>
2023-02-25 15:36:58 -0800

    ContactForm Part 2

--- Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/components/ContactForm.js
+++ Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/components/ContactForm.js
@@ -17,47 +17,56 @@ const ContactForm = () => {
 
             <Form>
                 <FormGroup row>
-                    <Label htmlFor='firstName' md='10'>
+                    <Label htmlFor='firstName' md='2'>
                         First Name
                     </Label>
-                    <Col></Col>
+                    <Col md='10'><Field name='firstName' className='form-control' placeholder='First Name' /></Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='lastName' md='10'>
+                    <Label htmlFor='lastName' md='2'>
                         Last Name
                     </Label>
-                    <Col></Col>
+                    <Col md='10'><Field name='lastName' className='form-control' placeholder="Last Name" /></Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='phoneNum' md='10'>
+                    <Label htmlFor='phoneNum' md='2'>
                         Phone Number
                     </Label>
-                    <Col></Col>
-
+                    <Col md='10'><Field name='phoneNum' className='form-control' placeholder='Phone Number' /></Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='email' md='10'>
+                    <Label htmlFor='email' md='2'>
                         Email
                     </Label>
-                    <Col></Col>
+                    <Col md='10'><Field name='email' className='form-control' placeholder='Email' /></Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label check md={{ size: 4, offset: 2 }}>
+                        <Field
+                            name='agree'
+                            type='checkbox'
+                            className='form-check-input'
+                        />{' '}
                         May we contact you?
                     </Label>
-                    <Col></Col>
+                    <Col md='4'>
+                        <Field name='contactType' as='select' className='form-control' >
+                            <option>By Phone</option>
+                            <option>By Email</option>
+                         </Field>
+                    </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='feedback' md='10'>
+                    <Label htmlFor='feedback' md='2'>
                         Feedback
                     </Label>
-                    <Col></Col>
+                    <Col md='10'><Field name='feedback' as='textarea' rows='12' className='form-control' /></Col>
                 </FormGroup>
                 <FormGroup row>
                 </FormGroup>
             </Form>
         </Formik>
-            )
+    )
 }
 
-            export default ContactForm;
+export default ContactForm;
