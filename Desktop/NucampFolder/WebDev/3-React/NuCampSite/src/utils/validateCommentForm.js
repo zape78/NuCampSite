34a5d94b8f91269b5278ddf5ffac3c8957be84f4
Brian Zapien <brianzapien@gmail.com>
2023-03-06 06:14:09 -0800

    addComment

--- Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/utils/validateCommentForm.js
+++ Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/utils/validateCommentForm.js
@@ -1,9 +1,9 @@
 export const validateCommentForm = (values) => {
     const errors = {};
-    
+
     if (!values.rating) {
         errors.rating = 'Required';
-    } 
+    }
     if (values.author.length < 2) {
         errors.author = 'Must be at least 2 characters';
     } else if (values.author.length > 15) {
