36ce5d2f9ebc978c7de3083269395af1d288cf74
Brian Zapien <116138741+zape78@users.noreply.github.com>
2024-01-11 01:09:26 -0800

    Delete Desktop/NucampFolder/WebDev directory
    Deleting directory

--- Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HomePage } from './HomePage';
-export { default as ContactPage } from './ContactPage';
-export { default as AboutPage } from './AboutPage';
-export { default as CampsitesDirectoryPage } from './CampsitesDirectoryPage';
-export { default as CampsiteDetailPage } from './CampsiteDetailPage';
--- Desktop/NucampFolder/WebDev/3-React/NuCampSite/src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date) => {
-    return new Intl.DateTimeFormat('en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: '2-digit'
-    }).format(new Date(Date.parse(date)));
-};
